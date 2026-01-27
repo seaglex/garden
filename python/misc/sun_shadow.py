cc6474f303aee2a19236d5c5f3f49f23e9e7ffb2
seaglex <seaglex@gmail.com>
2026-01-27 18:58:40 +0800

    done_feas: sun shadow

--- python/misc/sun_shadow.py
+++ python/misc/sun_shadow.py
@@ -1,68 +1,97 @@
+import matplotlib.pyplot as plt
 import datetime as dt
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-def get_sun_angle(alphas, beta, gamma):
+def transform(p: np.array):
     ETA = (23 + 26 / 60) / 180 * np.pi  # 黄赤交角
-    v = np.zeros((len(alphas), 3))
-    v[:, 0] = np.sin(beta) * np.cos(alphas) * np.cos(ETA) - np.cos(beta) * np.sin(ETA)
-    v[:, 1] = np.sin(beta)*np.sin(alphas)
-    v[:, 2] = np.cos(beta) * np.cos(ETA) + np.sin(beta) * np.cos(alphas) * np.sin(ETA)
-    w = np.array([np.cos(gamma), np.sin(gamma), 0])
-    results = v.dot(w) / np.sqrt( (v*v).sum(1) * w.dot(w) )
-    new_alphas = np.arctan2(v[:, 1], v[:, 0])
-    for i in range(1, len(alphas)):
-        if new_alphas[i] < new_alphas[i-1]:
-            new_alphas[i] += 2 * np.pi
-    return np.arccos(results), new_alphas
+    matrix = np.array([
+        [np.cos(ETA), 0, np.sin(ETA)],
+        [0, 1, 0],
+        [-np.sin(ETA), 0, np.cos(ETA)]
+    ])
+    return p.dot(matrix)
+
+def get_sun_angle(alpha, beta, gamma):
+    man = transform(np.array([np.sin(beta) * np.cos(alpha), np.sin(beta) * np.sin(alpha), np.cos(beta)]).transpose())
+    sun = np.array([np.cos(gamma), np.sin(gamma), gamma*0.0]).transpose()
+    results = np.sum(sun * man, axis=1) / (np.linalg.norm(sun, axis=1) * np.linalg.norm(man, axis=1))
+    return np.arccos(results)
+
+def get_sun_shadow(alpha, beta, gamma):
+    man = transform(np.array([np.sin(beta) * np.cos(alpha), np.sin(beta) * np.sin(alpha), np.cos(beta)]).transpose())
+    # north = - d man / dβ
+    north = transform(-np.array([np.cos(beta) * np.cos(alpha), np.cos(beta) * np.sin(alpha), -np.sin(beta)]).transpose())
+    shadow = -np.array([np.cos(gamma), np.sin(gamma), gamma*0.0]).transpose()
+    # 计算shadow在east/north上的投影
+    east = np.cross(north, man)
+    east = east / np.linalg.norm(east, axis=1).reshape(-1, 1)
+    y = np.sum(shadow * east, axis=1)
+    x = np.sum(shadow * north, axis=1)  ## the initial north is normalized
+    y[np.abs(y) <= 1e-10] = 0
+    x[np.abs(x) <= 1e-10] = 0
+    result = np.arctan2(y, x)
+    result[(y==0) & (x==0)] = np.nan
+    return result
 
 def draw_sun_angles():
-    gamma = np.pi*5/4  # (dt.date(2026, 1, 24) - dt.date(2025, 12, 21)).days / 365.25 * 2 * np.pi  # 太阳位置
+    latitude = 40
+    gamma = (dt.date(2026, 1, 24) - dt.date(2025, 12, 21)).days / 365.25 * 2 * np.pi  # 太阳位置
     alphas = np.arange(-30, 31) / 180 * np.pi + gamma
     xs = np.arange(-30, 31)/180 * 12 + 12  # 4 hours
-    beta = (90 - 40) / 180 * np.pi  # beijing N40
-    angles, new_alphas = get_angle(alphas, beta, gamma)
-    angles = angles / np.pi * 180
-    new_alphas = (new_alphas - gamma) / np.pi * 180
-    plt.plot(new_alphas, angles)
+    beta = (90 - latitude) / 180 * np.pi  # beijing N40
+    gamma = np.ones(alphas.shape) * gamma
+    beta = np.ones(alphas.shape) * beta
+    angles = get_sun_angle(alphas, beta, gamma) / np.pi * 180
+    shadow = get_sun_shadow(alphas, beta, gamma) / np.pi * 180
+    plt.plot(xs, angles)
+    plt.plot(xs, shadow)
+    I = np.argmin(angles)
+    plt.plot([xs[I], xs[I]], [0, 90])
+    plt.xlabel("hour")
+    plt.title(f"latitude {latitude}, gamma {gamma[0] / np.pi * 180}")
+    plt.legend(["min-angle", "shadow-direction"])
     plt.box(False)
     plt.grid(True)
     plt.savefig('sun_angles.png')
     plt.show()
 
-def transform(alpha, beta):
-    ETA = (23 + 26 / 60) / 180 * np.pi  # 黄赤交角
-    v0 = np.sin(beta) * np.cos(alpha) * np.cos(ETA) - np.cos(beta) * np.sin(ETA)
-    v1 = np.sin(beta)*np.sin(alpha)
-    v2 = np.cos(beta) * np.cos(ETA) + np.sin(beta) * np.cos(alpha) * np.sin(ETA)
-    return np.array([v0, v1, v2]).transpose()
 
-def get_sun_shadow(alpha, beta, gamma):
-    man = np.array(transform(alpha, beta))
-    east = np.array(transform(alpha + np.pi/18000, beta))
-    north = np.array(transform(alpha, beta - np.pi/18000))
-    east -= man
-    north -= man
-    shadow = -np.array([np.cos(gamma), np.sin(gamma), gamma*0.0]).transpose()
-    # 计算shadow在east和north组成平面的投影
-    z = np.cross(east, north)
-    shadow = shadow - np.sum(shadow * z, axis=1).reshape(-1, 1) * z / np.sum(z * z, axis=1).reshape(-1, 1)
-    # 计算shadow在north上的投影
-    s_north = np.sum(north * shadow, axis=1).reshape(-1, 1) * north / np.sum(north * north, axis=1).reshape(-1, 1)
-    return np.atan2(np.linalg.norm(shadow-s_north, axis=1), np.linalg.norm(s_north, axis=1))
-
-def draw_sun_shadow():
+def draw_sun_shadow_by_day():
+    latitude = 40
     gammas = np.arange(0, 360) / 180 * np.pi
     alphas = gammas
-    beta = (90 - 40) / 180 * np.pi  # beijing N40
+    beta = np.ones(gammas.shape) * (90 - latitude) / 180 * np.pi  # beijing N40
     angles = get_sun_shadow(alphas, beta, gammas)
     plt.plot(angles / np.pi * 180)
+    plt.title("latitude: %d°%d'" % (int(latitude), int((latitude - int(latitude)) * 60 + 0.5)))
     plt.box(False)
     plt.grid(True)
     plt.savefig('sun_shadow.png')
-    plt.show()
+    # plt.show()
+
+def draw_sun_shadow_in_one_day():
+    latitude0 = 60
+    latitude1 = 40
+    latitude2 = 20
+    alphas = np.arange(-60, 61) / (12 * 60) * np.pi
+    hours = np.arange(-60, 61) / 60 + 12
+    gammas = np.ones(alphas.shape) * np.pi
+    angles0 = get_sun_shadow(alphas + gammas, np.ones(gammas.shape) * (90 - latitude0) / 180 * np.pi, gammas)
+    angles1 = get_sun_shadow(alphas + gammas, np.ones(gammas.shape) * (90 - latitude1) / 180 * np.pi, gammas)
+    angles2 = get_sun_shadow(alphas + gammas, np.ones(gammas.shape) * (90 - latitude2) / 180 * np.pi, gammas)
+    plt.plot(hours, angles0 / np.pi * 180)
+    plt.plot(hours, angles1 / np.pi * 180)
+    plt.plot(hours, angles2 / np.pi * 180)
+    plt.title("shadow-direction(gamma %d)" % int(gammas[0] / np.pi * 180))
+    plt.xlabel("hour")
+    plt.legend(["latitude: %f" % latitude0, "latitude: %f" % latitude1, "latitude: %f" % latitude2])
+    plt.box(False)
+    plt.grid(True)
+    plt.savefig('sun_shadow.png')
+    # plt.show()
 
 
 if __name__ == '__main__':
-    draw_sun_shadow()
+    # draw_sun_shadow_in_one_day()
+    draw_sun_angles()
